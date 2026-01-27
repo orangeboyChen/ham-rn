badc1a8b117adfea12b4b38ad23fc45275eb34ff
orangeboyChen <chenenhan@qq.com>
2026-01-27 15:19:17 +0800

    feat(i18n): add localization setup and integrate translations across views

--- shell/push_ios.sh
+++
@@ -1,6 +0,0 @@
-cd ..
-rm -rf build
-mkdir build
-mkdir -p build/ios/CodePush
-react-native bundle --assets-dest build/ios/CodePush --bundle-output build/ios/CodePush/main.jsbundle --dev false --entry-file index.js --platform ios
-#appcenter codepush release-react -a orangeboy/ham_ios -m -t "*" -o build/ios
